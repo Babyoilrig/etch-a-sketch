d09a79316367574dc9e29fb7930468971529327e
babyoilrig <catrin.mentzoni@yahoo.com>
2023-08-19 11:03:28 +0100

    Plan for making an etch-a-sketch

---
+++ etch-a-sketch.js
@@ -0,0 +1,14 @@
+console.log('It works!');
+
+//Plan
+//Select elements on the page - canvas, shake button
+
+//Set up our canvas for drawing
+
+//Write a draw function 
+
+//Write a handler for the keys
+
+//Clear or shake function 
+
+//Listen for arrow keys
