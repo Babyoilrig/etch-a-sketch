b21e4c94008b9777c26282b417116d28bb37685d
babyoilrig <catrin.mentzoni@yahoo.com>
2023-08-19 11:14:46 +0100

    Selecting the shake button

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -2,8 +2,18 @@ console.log('It works!');
 
 //Plan
 //Select elements on the page - canvas, shake button
+
+//Selecting the canvas and console logging it 
 const canvas = document.querySelector('#etch-a-sketch');
-console.log(canvas);
+// console.log(canvas);
+//Selecting the content - i.e. the drawing element
+const ctx = canvas.getContext('2d');
+
+//Selecting the shake button
+//Select elements on the page - canvas, shake button
+const shakebutton = document.querySelector('.shake');
+console.log(shakebutton);
+
 //Set up our canvas for drawing
 
 //Write a draw function 
